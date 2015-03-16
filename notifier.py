55b72df4d81d9743fbdbe436583c6ed0f82d2715
Lazar-T <lazartelebak@gmail.com>
2015-03-16 17:40:39 +0100

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,12 +1,12 @@
 import time
 import pynotify
 
+
 pynotify.init("Pomodoro")
 
-start = pynotify.Notification("Pomodoro Technique",
+start = pynotify.Notification("Pomodoro Timer",
     """<i>The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s. The technique uses a timer to break down work into intervals traditionally 25 minutes in length, separated by short breaks. These intervals are known as "pomodori", the plural of the Italian word pomodoro for "tomato". The method is based on the idea that frequent breaks can improve mental agility. <a href="http://en.wikipedia.org/wiki/Pomodoro_Technique">Learn more</a></i>""",
-  "", #path to img.png
-)
+    "") #path to img.png
 
 start.show()
 
@@ -16,8 +16,14 @@ work_in_seconds = int(work_len) * 60
 pause_len = raw_input("Enter number of minutes between each pomodoro session: ")
 pause_in_seconds = int(pause_len) * 60
 
-pause = pynotify.Notification("Take break for " + pause_len + " minutes")
-work = pynotify.Notification("Start working now")
+pause = pynotify.Notification("Pomodoro Timer",
+    "Take break for " + pause_len + " minutes",
+    "") #path to img.png
+
+work = pynotify.Notification("Pomodoro Timer",
+    "Start working now",
+    "") #path to img.png
+
 
 while True:
     work.show()
