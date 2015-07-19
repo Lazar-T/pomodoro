758ce45ed5e5f73c1bb5aabf61d9deefa497b7e4
Lazar-T <lazartelebak@gmail.com>
2015-07-19 18:21:19 +0200

    pep8

--- notifier.py
+++ notifier.py
@@ -14,7 +14,8 @@ The technique uses a timer to break down work into intervals traditionally 25 mi
 by short breaks. These intervals are known as "pomodori", the plural of the Italian word pomodoro for "tomato".
 The method is based on the idea that frequent breaks can improve mental agility.
 <a href="http://en.wikipedia.org/wiki/Pomodoro_Technique">Learn more</a></i>""",
-os.getcwd() + "/img.png")
+os.getcwd() + "/img.png"
+)
 
 start.show()
 
@@ -24,14 +25,17 @@ work_in_seconds = int(work_len) * 60
 pause_len = raw_input("Enter number of minutes between each pomodoro session: ")
 pause_in_seconds = int(pause_len) * 60
 
-pause = pynotify.Notification("Pomodoro Timer",
+pause = pynotify.Notification(
+    "Pomodoro Timer",
     "Take break for " + pause_len + " minutes",
-    os.getcwd() + "/img.png")
+    os.getcwd() + "/img.png"
+)
 
-work = pynotify.Notification("Pomodoro Timer",
+work = pynotify.Notification(
+    "Pomodoro Timer",
     "Start working now",
-    os.getcwd() + "/img.png")
-
+    os.getcwd() + "/img.png"
+)
 
 while True:
     work.show()
